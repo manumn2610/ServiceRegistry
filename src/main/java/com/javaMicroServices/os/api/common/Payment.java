83b0b5a432d7a185edc41e37fa6631a0b4938634
manumn <manumn@localhost>
2022-10-26 09:27:32 -0400

    Test comomit

--- src/main/java/com/javaMicroServices/os/api/common/Payment.java
+++ src/main/java/com/javaMicroServices/os/api/common/Payment.java
@@ -18,6 +18,42 @@ public class Payment {
 	@Id
 	@GeneratedValue
 	private int paymentId;
+	public int getPaymentId() {
+		return paymentId;
+	}
+	public void setPaymentId(int paymentId) {
+		this.paymentId = paymentId;
+	}
+	public String getPaymentStatus() {
+		return paymentStatus;
+	}
+	public void setPaymentStatus(String paymentStatus) {
+		this.paymentStatus = paymentStatus;
+	}
+	public String getTransactionId() {
+		return transactionId;
+	}
+	public void setTransactionId(String transactionId) {
+		this.transactionId = transactionId;
+	}
+	public int getOrderId() {
+		return orderId;
+	}
+	public void setOrderId(int orderId) {
+		this.orderId = orderId;
+	}
+	public double getAmount() {
+		return amount;
+	}
+	public void setAmount(double amount) {
+		this.amount = amount;
+	}
+	public String getPort() {
+		return port;
+	}
+	public void setPort(String port) {
+		this.port = port;
+	}
 	private String paymentStatus;
 	private String transactionId;
 	private int orderId;
